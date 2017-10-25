a814aa508e8c9351bda5f7273130e78715b6557f
Loris <loris4526@gmail.com>
2017-10-25 14:18:04 +0200

    Serve static files. Set 'ejs' as default templating engine

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const Koa = require('koa'),
       Router = require('koa-router'),
-      bodyParser = require('koa-body')
+      bodyParser = require('koa-body'),
+      render = require('koa-ejs'),
+      path = require('path')
 
 const app = new Koa()
 
@@ -17,14 +19,30 @@ app.use(async (ctx, next) => {
 })
 
 
+//TEMPLATING ENGINE
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: false,
+  viewExt: 'ejs',
+  cache: false,
+  debug: false
+})
+
 //MIDDLEWARES
 app.use(bodyParser())
+
+//SERVE STATIC FILES
+app.use(require('koa-static-server')({
+  rootDir: 'static',
+  rootPath: '/static'
+}))
 
 //ROUTER
 const router = new Router()
 
 router.get('/', async (ctx)=>{
-  ctx.body = 'Hello World'
+  // ctx.body = "Hello There"
+  await ctx.render('home')
 })
 
 router.post('/postroute', async (ctx)=>{
